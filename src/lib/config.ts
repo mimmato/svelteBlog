152dec6a9d25b2d69f962efd4f2745b3f66373aa
mimmato <mimmato@gmail.com>
2025-04-15 18:17:32 +0300

    updating CONTACT page for better styling

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
-export const title = 'Articles'
+export const title = 'Mihail M. Blog posts'
 export const description = 'Mihail M. Portfolio'
 export const url = dev ? 'http://localhost:5173' : 'github pages url'
