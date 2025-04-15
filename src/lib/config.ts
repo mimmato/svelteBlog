e9aa34ac50896b17559d5c7ca4689dae7c97080d
mimmato <mimmato@gmail.com>
2025-04-15 19:07:15 +0300

    updating CONTACT page for better styling

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'Mihail M. Blog posts'
 export const description = 'Mihail M. Portfolio'
-export const url = dev ? 'http://localhost:5173' : 'https://mimmato-svelte.vercel.app'
+export const url = dev ? 'http://localhost:5173' : 'https://mimmato.vercel.app'
